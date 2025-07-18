a8eb67e7beec47ec2c7ada28b1d1ebd0a7777303
Omkar Sawant <omkar.s@neosoftmail.com>
2025-07-18 15:53:46 +0530

    - changes

--- composeApp/src/commonMain/kotlin/com/app/portfolio/App.kt
+++ composeApp/src/commonMain/kotlin/com/app/portfolio/App.kt
@@ -3,13 +3,12 @@ package com.app.portfolio
 import PortfolioApp
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import com.app.ui.theme.LightColorScheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme(LightColorScheme) {
+    MaterialTheme {
         PortfolioApp()
     }
 }
