53f4e1cecca74162c434a2bb36a0c50f72b634b2
Omkar Sawant <omkar.s@neosoftmail.com>
2025-07-18 15:38:31 +0530

    - changes

--- composeApp/src/commonMain/kotlin/com/app/ui/theme/Color.kt
+++ composeApp/src/commonMain/kotlin/com/app/ui/theme/Color.kt
@@ -2,11 +2,11 @@ package com.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Primary = Color(0xff076fb9)
+val Primary = Color(0xffa80000)
 val Secondary = Color(0xff093b5d)
 val Tertiary = Color(0xffe1ddd9)
 
-val PrimaryDark = Color(0xff076fb9)
+val PrimaryDark = Color(0xffd83831)
 val SecondaryDark = Color(0xfcded7d7)
 val TertiaryDark = Color(0xffe1ddd9)
 
